156492ebf40206f4659442e47d3fb18fee256e73
luisromo01 <lromo400@gmail.com>
2021-12-15 11:17:23 -0600

    Update app.py
    added imports
    added input prompt for file location
    printing image size

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 #3.5 inch = 336 pixels
 #2.5 inch = 240 pixels
+from PIL import Image
+import os
+import PIL
+import glob
+
+loc = input("Enter file location")
+image = Image.open(loc) 
+print(image.size)
+
+
