b8fc068fe825f182214bafc6c64fc5771ebcf5c8
luisromo01 <lromo400@gmail.com>
2021-12-15 14:58:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,31 @@ import PIL
 import glob
 
 loc = input("Enter file location")
-image = Image.open(loc) 
-print(image.size)
+files = os.listdir(loc)
+images = [Image.open(x) for x in files]
+total_width = 0
+max_height = 0
+# find the width and height of the final image
+for img in images:
+    #resize first
+    img.resize(240,336)
+    total_width += img.size[0]
+    max_height = max(max_height, img.size[1])
+    
+print(total_width + ', ' + ma_height)
 
-resized_image = image.resize((240,336)) 
-print(resized_image.size)
-resized_image.show()
-#Source: How to Resize an Image in Python (+ Examples) - Dopinger (https://blog.dopinger.com/how-to-resize-an-image-in-python)
-
-#turn image into pdf
-im1 = resized_image.convert('RGB')
-dest = input("Enter location to save")
-im1.save(dest)
+#resized_image = image.resize((240,336)) 
+#print(resized_image.size)
+#resized_image.show()
+##Source: How to Resize an Image in Python (+ Examples) - Dopinger (https://blog.dopinger.com/how-to-resize-an-image-in-python)
+#images = [Image.open(x) for x in ['path/to/image1', '/path/to/image2']]
+#total_width = 0
+#max_height = 0
+## find the width and height of the final image
+#for img in images:
+#    total_width += img.size[0]
+#    max_height = max(max_height, img.size[1])
+##turn image into pdf
+#im1 = resized_image.convert('RGB')
+#dest = input("Enter location to save")
+#im1.save(dest)
