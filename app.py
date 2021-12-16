b8df504bc543dd6fb35ddb2394d738caea3e7486
luisromo01 <lromo400@gmail.com>
2021-12-16 10:24:18 -0600

    Update app.py
    added check for if there are 3 pics in one row then that gets added to a list of pictures

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #3.5 inch = 336 pixels
 #2.5 inch = 240 pixels
 #3 x 1 = 720 wide
+#note for incomplete pages. have a 2.5 x3.5 white image. copy it the number\
+#  of times needed to get to 9 then paste those white images so we have 9 pics left
 from PIL import Image
 import os
 import PIL
@@ -22,13 +24,15 @@ for i in range(len(images)):
     
 print(str(total_width) + ', ' + str(max_height))
 
-# create a new image with the appropriate height and width
+# create 3x1 new image with the appropriate height and width
 new_img = Image.new('RGB', (total_width, max_height))
 # Write the contents of the new image
 current_width = 0
 for img in images:
   new_img.paste(img, (current_width,0))
   current_width += img.size[0]
+  if total_width == 720:
+    threepics.append(new_img)
 # Save the image
 new_img.save('NewImage.jpg')
 
